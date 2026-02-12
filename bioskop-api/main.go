1e3448145f7dd62e10fcb0beb70c35aa140d6412
shafarisqiocta <risqishafa@gmail.com>
2026-02-12 16:13:49 +0700

    updated request

--- bioskop-api/main.go
+++ bioskop-api/main.go
@@ -16,6 +16,7 @@ type Bioskop struct {
 
 func main() {
 	r := gin.Default()
+	//req POST
 	r.POST("/bioskop", func(c *gin.Context) {
 		var input Bioskop
 
@@ -47,6 +48,88 @@ func main() {
 		}
 
 		c.JSON(http.StatusOK, input)
+	})
+	//req GET all data
+	r.GET("/bioskop", func(c *gin.Context) {
+		rows, err := config.DB.Query("SELECT id,nama,lokasi,rating FROM bioskop")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		defer rows.Close()
+		var bioskops []Bioskop
+
+		for rows.Next() {
+			var b Bioskop
+			err := rows.Scan(&b.ID, &b.Nama, &b.Lokasi, &b.Rating)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			bioskops = append(bioskops, b)
+		}
+		c.JSON(http.StatusOK, bioskops)
+	})
+	//req GET berdasarkan ID
+	r.GET("/bioskop/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		var b Bioskop
+		query := "SELECT id, nama, lokasi, rating FROM bioskop WHERE id=$1"
+		err := config.DB.QueryRow(query, id).Scan(&b.ID, &b.Nama, &b.Lokasi, &b.Rating)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Data tidak ditemukan"})
+			return
+		}
+		c.JSON(http.StatusOK, b)
+	})
+	//req Update
+	r.PUT("bioskop/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		var input Bioskop
+		if err := c.ShouldBindBodyWithJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if input.Nama == "" || input.Lokasi == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Nama dan Lokasi tidak boleh kosong",
+			})
+			return
+		}
+
+		query := `UPDATE bioskop SET nama=$1, lokasi=$2, rating=$3 WHERE id=$4`
+
+		result, err := config.DB.Exec(query, input.Nama, input.Lokasi, input.Rating, id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		rowsAffected, _ := result.RowsAffected()
+		if rowsAffected == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Data tidak ditemukan"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Data berhasil diupdate"})
+
+	})
+	//Req DELETE
+	r.DELETE("/bioskop/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		result, err := config.DB.Exec("DELETE FROM bioskop WHERE id=$1", id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		rowsAffected, _ := result.RowsAffected()
+		if rowsAffected == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Data tidak ditemukan"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"Message": "Data berhasil dihapus"})
+
 	})
 
 	config.ConnectDB()
